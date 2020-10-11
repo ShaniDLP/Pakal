0d9fa865fa7dea5cfb9cd94a6402672039a3ca8e
shanipj <shanipj27@gmail.com>
2020-10-11 11:44:33 +0300

    clean unnecessary code

--- src/Components/Navigation/ToolBar/Toolbar.js
+++ src/Components/Navigation/ToolBar/Toolbar.js
@@ -7,17 +7,12 @@ import {
 } from "react-router-dom";
 import 'w3-css/w3.css';
 import Navigation from './Navigation';
-import Sidebar from './SideBar';
-import Logo from '../../UI/Logo/Logo';
+// import Sidebar from './SideBar';
 
 const toolbar = ({ onClick, areaname, areaKey }) => (
   <div>
     <header className="Toolbar">
-
-   <Navigation/>
-   
- 
-  
+      <Navigation />
       <h1>פקל קפה</h1>
     </header>
   </div>
@@ -25,13 +20,3 @@ const toolbar = ({ onClick, areaname, areaKey }) => (
 
 export default toolbar;
 
-// כרגע אין שימוש בתיקיית של navigation
-
-// <ul> 
-// <li> <Link to="/area=north" areaname="north" data-areaKey={0} >צפון</Link></li>
-// <li> <Link to="/area=center" areaname="center" data-areaKey={1} >מרכז</Link></li>
-// <li> <Link to="/area=south" areaname="south" data-areaKey={2}>דרום</Link></li>
-// <li> <Link to="/info">?הידעת</Link></li>
-// <li> <Link to="/sendform">שלח לנו המלצה</Link></li>
-// </ul>
- 
