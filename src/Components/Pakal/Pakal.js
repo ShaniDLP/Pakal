206f913f990c2a2b9f630a839eb8a02a8871c113
shanipj <shanipj27@gmail.com>
2021-01-19 01:53:47 +0200

    update router BrowserRouter with basement

--- src/Components/Pakal/Pakal.js
+++ src/Components/Pakal/Pakal.js
@@ -4,67 +4,65 @@ import Landing from '../Pages/Landing/Landing'
 import Sites from '../Pages/Sites/Sites';
 import Sendform from '../Pages/Sendform/Sendform';
 import Info from '../Pages/Info/Info';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-    HashRouter as Router,
-    Route
+  Route
 } from "react-router-dom";
 
 
 class Pakal extends Component {
 
-    state = {
-        sideBar: true
-    }
-    //when user click on menu icon close/open side bar
-  showSideBar = (e) =>{
+  state = {
+    sideBar: true
+  }
+  //when user click on menu icon close/open side bar
+  showSideBar = (e) => {
     e.preventDefault();
     console.log('sidebar!!!');
-    if(this.state.sideBar){
-      this.setState({sideBar : false})
+    if (this.state.sideBar) {
+      this.setState({ sideBar: false })
     }
-    else {this.setState({sideBar : true})}
+    else { this.setState({ sideBar: true }) }
     console.log(this.state.sideBar);
   }
-//close the sidebar when user click on backdrop
+  //close the sidebar when user click on backdrop
   closeSideBar = () => {
     console.log('backdrop clicked!');
     this.setState({ sideBar: true })
   }
 
-    render() {
-        return (
-          
-                <Router>
-                    <Route path="/" exact >
-                        <Landing
-                        showSideBar= {this.showSideBar}
-                        closeSideBar= {this.closeSideBar}
-                        sideBar= {this.state.sideBar} />
-                    </Route>
-                    <Route path="/area=:areaName" >
-                        <Sites 
-                            showSideBar= {this.showSideBar}
-                            closeSideBar= {this.closeSideBar}
-                            sideBar= {this.state.sideBar} />
-                    </Route>
-                    <Route path="/sendform">
-                        <Sendform
-                         showSideBar= {this.showSideBar}
-                        closeSideBar= {this.closeSideBar}
-                        sideBar= {this.state.sideBar} />
-                    </Route>
-                    <Route path="/info">
-                        <Info 
-                        showSideBar= {this.showSideBar}
-                        closeSideBar= {this.closeSideBar}
-                        sideBar= {this.state.sideBar}/>
-                    </Route>
-                </Router>
-           
-     );
-    }
+  render() {
+    return (
+      <Router basename={process.env.PUBLIC_URL}>
+        <Route path="/" exact >
+          <Landing
+            showSideBar={this.showSideBar}
+            closeSideBar={this.closeSideBar}
+            sideBar={this.state.sideBar} />
+        </Route>
+        <Route path="/area=:areaName" >
+          <Sites
+            showSideBar={this.showSideBar}
+            closeSideBar={this.closeSideBar}
+            sideBar={this.state.sideBar} />
+        </Route>
+        <Route path="/sendform">
+          <Sendform
+            showSideBar={this.showSideBar}
+            closeSideBar={this.closeSideBar}
+            sideBar={this.state.sideBar} />
+        </Route>
+        <Route path="/info">
+          <Info
+            showSideBar={this.showSideBar}
+            closeSideBar={this.closeSideBar}
+            sideBar={this.state.sideBar} />
+        </Route>
+      </Router>
+
+    );
+  }
 }
 export default Pakal;
 
